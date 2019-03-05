146b21558c7012d9be35f8c13098be5c6eb9d13c
LawAng <tong.not.dead@gmail.com>
2019-03-05 12:29:30 +0700

    Initial commit

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,16 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+
+    private String urlAddUser = "http://www.androidthai.in.th/bsru/addUserMaster.php";
+    private String urlGetUserWhereUser = "http://www.androidthai.in.th/bsru/getUserWhereUserPhakphum.php";
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
